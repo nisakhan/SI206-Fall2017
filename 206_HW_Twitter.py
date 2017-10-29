a27fc83656ec8e8b2f8a8bcb0322b56f0fac9aea
Work Account <Workaccount@Nisas-MacBook-Pro.local>
2017-10-29 17:38:05 -0400

    Not done yet

--- 206_HW_Twitter.py
+++ 206_HW_Twitter.py
@@ -9,22 +9,22 @@ import json
 ## Any names of people you worked with on this assignment:
 
 
-## Write code that uses the tweepy library to search for tweets with three different phrases of the 
-## user's choice (should use the Python input function), and prints out the Tweet text and the 
-## created_at value (note that this will be in GMT time) of the first FIVE tweets with at least 
+## Write code that uses the tweepy library to search for tweets with three different phrases of the
+## user's choice (should use the Python input function), and prints out the Tweet text and the
+## created_at value (note that this will be in GMT time) of the first FIVE tweets with at least
 ## 1 blank line in between each of them, e.g.
 
 
-## You should cache all of the data from this exercise in a file, and submit the cache file 
-## along with your assignment. 
+## You should cache all of the data from this exercise in a file, and submit the cache file
+## along with your assignment.
 
-## So, for example, if you submit your assignment files, and you have already searched for tweets 
-## about "rock climbing", when we run your code, the code should use CACHED data, and should not 
-## need to make any new request to the Twitter API.  But if, for instance, you have never 
-## searched for "bicycles" before you submitted your final files, then if we enter "bicycles" 
+## So, for example, if you submit your assignment files, and you have already searched for tweets
+## about "rock climbing", when we run your code, the code should use CACHED data, and should not
+## need to make any new request to the Twitter API.  But if, for instance, you have never
+## searched for "bicycles" before you submitted your final files, then if we enter "bicycles"
 ## when we run your code, it _should_ make a request to the Twitter API.
 
-## Because it is dependent on user input, there are no unit tests for this -- we will 
+## Because it is dependent on user input, there are no unit tests for this -- we will
 ## run your assignments in a batch to grade them!
 
 ## We've provided some starter code below, like what is in the class tweepy examples.
@@ -34,56 +34,56 @@ import json
 
 
 
-## **** For extra credit, create another file called twitter_info.py that 
-## contains your consumer_key, consumer_secret, access_token, and access_token_secret, 
+## **** For extra credit, create another file called twitter_info.py that
+## contains your consumer_key, consumer_secret, access_token, and access_token_secret,
 ## import that file here.  Do NOT add and commit that file to a public GitHub repository.
 
-## **** If you choose not to do that, we strongly advise using authentication information 
-## for an 'extra' Twitter account you make just for this class, and not your personal 
-## account, because it's not ideal to share your authentication information for a real 
+## **** If you choose not to do that, we strongly advise using authentication information
+## for an 'extra' Twitter account you make just for this class, and not your personal
+## account, because it's not ideal to share your authentication information for a real
 ## account that you use frequently.
 
-## Get your secret values to authenticate to Twitter. You may replace each of these 
-## with variables rather than filling in the empty strings if you choose to do the secure way 
+## Get your secret values to authenticate to Twitter. You may replace each of these
+## with variables rather than filling in the empty strings if you choose to do the secure way
 ## for EC points
-consumer_key = "" 
-consumer_secret = ""
-access_token = ""
-access_token_secret = ""
+consumer_key = "TMsbGZML5iwPtPqYIFlktBSZO"
+consumer_secret = "duLMmk0Y0WfNhbbO0ZfH8rzlCo9ybizHK2R2KE9t7mgzB5awbq"
+access_token = "920735606958886912-1Dj7G8j9qjFsGfkUANFYzrH2pJQq1eu"
+access_token_secret = "PoLp1P5fI4zRXe5BGvAEyqyTDzxPhpDrezOvDzjpTxdGX"
 ## Set up your authentication to Twitter
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-# Set up library to grab stuff from twitter with your authentication, and 
+# Set up library to grab stuff from twitter with your authentication, and
 # return it in a JSON-formatted way
 
-api = tweepy.API(auth, parser=tweepy.parsers.JSONParser()) 
+api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 
 ## Write the rest of your code here!
 
 #### Recommended order of tasks: ####
-## 1. Set up the caching pattern start -- the dictionary and the try/except 
+## 1. Set up the caching pattern start -- the dictionary and the try/except
 ## 		statement shown in class.
 
+try:
+    fo = open("my_feed.json")
+    my_posts_raw = fo.read()
+    posts_in_json = json.loads(my_posts_raw)
+    fo.close()
+except:
+    post_in_json = {}
 
+## 2. Write a function to get twitter data that works with the caching pattern,
+## 		so it either gets new data or caches data, depending upon what the input
+##		to search for is.
 
-## 2. Write a function to get twitter data that works with the caching pattern, 
-## 		so it either gets new data or caches data, depending upon what the input 
-##		to search for is. 
+def get_t(search_term):
+    data = json.loads(search_term)
+    print(data)
 
-
-
-## 3. Using a loop, invoke your function, save the return value in a variable, and explore the 
+## 3. Using a loop, invoke your function, save the return value in a variable, and explore the
 ##		data you got back!
 
 
-## 4. With what you learn from the data -- e.g. how exactly to find the 
-##		text of each tweet in the big nested structure -- write code to print out 
+## 4. With what you learn from the data -- e.g. how exactly to find the
+##		text of each tweet in the big nested structure -- write code to print out
 ## 		content from 5 tweets, as shown in the linked example.
-
-
-
-
-
-
-
-
